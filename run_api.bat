979398df8a5cd2d41b226996f13589edf3668d4a
phongtran <phongtrannm@gmail.com>
2025-04-22 11:43:44 +0700

    api and public port

--- run_api.bat
+++ run_api.bat
@@ -1,4 +1,13 @@
 @echo off
-echo Running FastAPI server from: %CD%\api_server.py
-uvicorn api_server:app --host 0.0.0.0 --port 8881 --reload
-pause
+setlocal
+
+REM Chạy FastAPI server (chạy ngầm bằng start)
+start "" uvicorn api_server:app --host 0.0.0.0 --port 8000 --reload
+
+REM Chờ 3 giây để chắc chắn API đã start
+timeout /t 3 /nobreak >nul
+
+REM Chạy public_port.py
+python D:\source-dev\setup\reverse_tunnel_client\src\public_port\public_port.py
+
+endlocal
